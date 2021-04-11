07faa74c28f535b49274406152744e6513bed372
hungnguyen <hung.nguyen@wesport.zone>
2021-04-11 21:18:44 +0700

    no message

--- flame_alarm/flame_alarm.ino
+++ flame_alarm/flame_alarm.ino
@@ -2,6 +2,8 @@
    Alarm fire
 
 */
+#include <Servo.h>
+Servo servo;
 
 #define STATE_STARTUP 0
 #define STATE_TRACKING 1
@@ -10,26 +12,40 @@
 #define STATE_PUMP 4
 byte currentState = STATE_STARTUP;
 
-int flameD0 = 2;
-int btStop = 3;
+int flameD0 = 4;
+int btStop = 5;
+int tempPin = A0;
 int buzzer = 8;
-int pumpPin = 9;
-int servoPin = 10;
+int servoPin = 9;
+int pumpPin = 10;
 
-int temp = 30;
+//for temp ntc b3950
+int Vo;
+float R1 = 10000;
+float logR2, R2, T, temp;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+float tempFire = 40;
+//for servo
+int servoPos1 = 0;
+int servoPos2 = 90;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  servo.attach(servoPin);
+
   pinMode(flameD0, INPUT);
   pinMode(btStop, INPUT_PULLUP);
   pinMode(buzzer, OUTPUT);
   pinMode(pumpPin, OUTPUT);
   pinMode(servoPin, OUTPUT);
+  pinMode(tempPin, INPUT);
 
-  digitalWrite(buzzer,LOW);
-  digitalWrite(pumpPin,LOW);
-  
+  digitalWrite(buzzer, LOW);
+  digitalWrite(pumpPin, HIGH);
+  servo.write(servoPos1);
+
   updateState(STATE_TRACKING);
 }
 
@@ -74,8 +90,9 @@ void updateState(byte aState)
 }
 //--------------------------------
 void tracking() {
+  digitalWrite(buzzer, LOW);
+  servo.write(servoPos1);
   while (true) {
-    Serial.println(digitalRead(flameD0));
     if (digitalRead(flameD0) == 1) {
       updateState(STATE_FIRE);
       break;
@@ -85,31 +102,79 @@ void tracking() {
 void fire() {
   long t = millis();
   while (millis() - t < 3000) {}
+  temp = get_temp();
+  Serial.print(digitalRead(flameD0));
+  Serial.print("\t");
+  Serial.println(temp);
   if ( digitalRead(flameD0) == 1) {
-    //move servo up
-    //buzzer on
+    servo.write(servoPos2);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
     updateState(STATE_CHECKFIRE);
   } else {
     updateState(STATE_TRACKING);
+  }
+  if (temp > tempFire)  {
+    updateState(STATE_PUMP);
   }
 }
 
 void checkfire() {
   long t = millis();
-  while (millis() - t < 30000) {
-    if (temp > 50)  {
+  while (millis() - t < 10000) {
+    temp = get_temp();
+
+    if (temp > tempFire)  {
+      Serial.println(temp);
       updateState(STATE_PUMP);
       break;
-    } else if (btStop == 0) {
+    }
+    if (digitalRead(btStop) == 0) {
       updateState(STATE_TRACKING);
       break;
-    } else {
-      
     }
   }
+  if ( digitalRead(flameD0) == 1) {
+    //warning to SIM
+    Serial.println("SIM WARNING FIRE");
+  }
+  while (millis() - t < 15000) {
+    temp = get_temp();
+    //    Serial.println(temp);
+    if (temp > tempFire)  {
+      Serial.println(temp);
+      updateState(STATE_PUMP);
+      break;
+    }
+    if (digitalRead(btStop) == 0) {
+      updateState(STATE_TRACKING);
+      break;
+    }
+  }
+  Serial.println("SIM not fire");
+  servo.write(servoPos1);
+  delay(1000);
+  updateState(STATE_TRACKING);
 }
 
 void pump() {
-  delay(5000);
+  Serial.println("SIM Killing fire");
+//  senMsg("SIM Killing fire");
+  while (temp > tempFire || digitalRead(flameD0) == 1) {
+    temp = get_temp();
+    digitalWrite(pumpPin, LOW);
+  }
+  digitalWrite(pumpPin, HIGH);
+  Serial.println("SIM Kill fire done");
+  Serial.println(temp);
+  delay(1000);
   updateState(STATE_TRACKING);
+}
+
+float get_temp() {
+  Vo = analogRead(tempPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+  return T - 273.15; //độ C
 }
