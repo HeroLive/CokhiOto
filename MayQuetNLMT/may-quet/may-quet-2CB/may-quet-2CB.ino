189c119ff1e7100ef9295b4a7ddfa8f25835602a
Hero Live <herolivechannel@gmail.com>
2022-03-09 17:38:20 +0700

    May quet 2CB: add speed by pot

--- MayQuetNLMT/may-quet/may-quet-2CB/may-quet-2CB.ino
+++ MayQuetNLMT/may-quet/may-quet-2CB/may-quet-2CB.ino
@@ -1,36 +1,75 @@
 // Run a A4998 Stepstick from an Arduino UNO.
-int en = 8 ;
-int dirPinX = 5 ;
-int stepPinX = 2 ;
-int endPinX = 9;
-bool dirX = LOW;
-int lastEndX = 1;
+int stepPinX = 10 ;
+int dirPinX = 11 ;
+int en = 12 ;
+int endPinXA = 8;
+int endPinXB = 9;
+int pinPot = A7;
 
-void setup() 
+bool dirX = LOW;
+bool startDirX = LOW;
+bool lastEndXA = HIGH;
+bool lastEndXB = HIGH;
+
+int curPot = 0;
+int lastPot = 0;
+int stepsPerUnit = 800;
+int rpm = 0;
+float curSpeed = 0;
+float period = 0;
+
+
+void setup()
 {
   Serial.begin(9600);
-  pinMode(en,OUTPUT); // Enable
-  pinMode(stepPinX,OUTPUT); // Step
-  pinMode(dirPinX,OUTPUT); // Dir
-  pinMode(endPinX,INPUT_PULLUP);
-  digitalWrite(en,LOW); // Set Enable low
-  digitalWrite(dirPinX,dirX);
+  pinMode(en, OUTPUT); // Enable
+  pinMode(stepPinX, OUTPUT); // Step
+  pinMode(dirPinX, OUTPUT); // Dir
+  pinMode(endPinXA, INPUT_PULLUP);
+  pinMode(endPinXB, INPUT_PULLUP);
+  digitalWrite(en, LOW); // Set Enable low
+  digitalWrite(dirPinX, dirX);
 }
 
 void loop() {
-  Serial.println(digitalRead(endPinX));
-  
-  if (digitalRead(endPinX)!= lastEndX){
-    if(digitalRead(endPinX)==0){
-       lastEndX = 0;
-       dirX = !dirX; 
-       digitalWrite(dirPinX,dirX);
+  //  Serial.println(digitalRead(endPinX));
+  curPot = analogRead(pinPot);
+  if (abs(curPot - lastPot) > 10) {
+    lastPot = curPot;
+    rpm = map(curPot, 0, 1023, 0, 250);
+    curSpeed = rpm / 60.0 * stepsPerUnit;
+    period = 1.0 / curSpeed * 1000000;
+//    Serial.print("curPot:");
+//    Serial.print(curPot);
+//    Serial.print(" rpm:");
+//    Serial.print(rpm);
+//    Serial.print(" period:");
+//    Serial.println(period);
+  }
+
+  if (digitalRead(endPinXA) != lastEndXA) {
+    if (digitalRead(endPinXA) == 0) {
+      lastEndXA = LOW;
+      dirX = !startDirX;
+      digitalWrite(dirPinX, dirX);
     } else {
-      lastEndX = 1;
+      lastEndXA = 1;
     }
   }
-    digitalWrite(stepPinX,HIGH);
-    delay(1);
-    digitalWrite(stepPinX,LOW);
-    delay(1);
+  if (digitalRead(endPinXB) != lastEndXB) {
+    if (digitalRead(endPinXB) == 0) {
+      lastEndXB = LOW;
+      dirX = startDirX;
+      digitalWrite(dirPinX, dirX);
+    } else {
+      lastEndXB = 1;
+    }
+  }
+  if (rpm > 1) {
+    digitalWrite(stepPinX, HIGH);
+    delayMicroseconds(0.4 * period);
+    digitalWrite(stepPinX, LOW);
+    delayMicroseconds(0.5 * period);
+  }
+
 }
