f7d5a1a7c7f223af5d48566bf3a5625607c1bfd8
Hero Live <66154935+HeroLive@users.noreply.github.com>
2023-04-13 22:27:01 +0700

    may 4 truc  add 4 truc

--- MayBom4Step/MayBom4Step.ino
+++ MayBom4Step/MayBom4Step.ino
@@ -7,17 +7,16 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // or 0x3F
 
-StaticJsonDocument<128> StepperDoc;
 StaticJsonDocument<256> DataDoc;
 
 #define BT1_PIN 9
 #define MODE 10
 #define DOWN 11
 #define UP 12
-#define PUL1_PIN 5
-#define PUL2_PIN 6
-#define PUL3_PIN 7
-#define PUL4_PIN 8
+#define PUL1_PIN 4
+#define PUL2_PIN 5
+#define PUL3_PIN 6
+#define PUL4_PIN 7
 #define DIR1_PIN 13
 #define DIR2_PIN 2
 StepperMotor motor_01(PUL1_PIN, DIR1_PIN);
@@ -32,17 +31,36 @@ bool M1 = false;
 bool M2 = false;
 bool M3 = false;
 bool M4 = false;
-long D200 = 3000;  //motor 2 frequency
-long D202 = 8000;  //motor 2 pulse value
-long D204 = 5000;  //motor 2 interval time
-long D0 = 0;       //target position
+//motor pulse value
+long D200 = 100;
+long D220 = 200;
+long D240 = 300;
+long D260 = 400;
+//motor frequency
+long D202 = 3000;
+long D222 = 3000;
+long D242 = 3000;
+long D262 = 3000;
+//motor interval time
+long D204 = 5;
+long D224 = 5;
+long D244 = 5;
+long D264 = 5;
+//target position
+long D0 = 0;
+long D20 = 0;
+long D40 = 0;
+long D60 = 0;
+//
 long _t1 = millis();
 long _t2 = millis();
 long _t3 = millis();
 long _t4 = millis();
+long _t_Press = millis();
+;
 
 String menu[] = { "So xung M1", "Toc do M1", "Thoi gian M1", "So xung M2", "Toc do M2", "Thoi gian M2", "So xung M3", "Toc do M3", "Thoi gian M3", "So xung M4", "Toc do M4", "Thoi gian M4" };
-long menuValue[] = { 8000, 3000, 5, 1020, 220, 2, 1030, 230, 3, 1040, 240, 4 };
+long menuValue[] = { D200, D202, D204, D220, D222, D224, D240, D242, D244, D260, D262, D264 };
 int menuLen = 12;
 
 void setup() {
@@ -54,9 +72,11 @@ void setup() {
   pinMode(UP, INPUT_PULLUP);
   pinMode(DOWN, INPUT_PULLUP);
 
+
+  //sweep data in EEPROM
+  // sweepData();
+
   //read data from EEPROM
-
-
   readData();
 
   //For OLED I2C
@@ -69,26 +89,31 @@ void setup() {
   delay(1000);
 
   M1 = true;
-  D0 = D202;
-
+  M2 = true;
+  M3 = true;
+  M4 = true;
   lcd.clear();
 }
 
 void loop() {
   if (digitalRead(BT1_PIN) == 0) {
+    
     if (digitalRead(MODE) == 0) {
+      lcd.clear();
       Serial.println(state);
       while (digitalRead(MODE) == 0)
         ;
       state = state + 1;
-      lcd.clear();
     }
 
     Serial.print(menu[state]);
     Serial.println(menuValue[state]);
     if (state == menuLen) {
+      sweepData();
+      delay(1000);
       writeData();
       state = 0;
+      delay(1000);
     } else {
       updateState(state);
     }
@@ -137,6 +162,13 @@ void loop() {
   }
 }
 
+void displayLCD(){
+  lcd.setCursor(0, 0);
+  lcd.print("Dang cho");
+  lcd.setCursor(0, 1);
+  lcd.print(".....");
+}
+
 void updateState(int i) {
   // do state change
   lcd.setCursor(0, 0);
@@ -144,14 +176,16 @@ void updateState(int i) {
   lcd.setCursor(0, 1);
   lcd.print(menuValue[i]);
   if (digitalRead(DOWN) == 0) {
-    while (digitalRead(DOWN) == 0)
-      ;
+    while (digitalRead(DOWN) == 0) {
+    }
+
     menuValue[i] = menuValue[i] <= 0 ? 0 : menuValue[i] - 1;
     lcd.clear();
   }
   if (digitalRead(UP) == 0) {
-    while (digitalRead(UP) == 0)
-      ;
+
+    while (digitalRead(UP) == 0) {
+    }
     menuValue[i] = menuValue[i] + 1;
     lcd.clear();
   }
@@ -163,11 +197,20 @@ void readData() {
   deserializeJson(DataDoc, eepromStream);
   if (debug) serializeJsonPretty(DataDoc, Serial);
   if (DataDoc["isStore"] == 1) {
-    // deserializeJson(StepperDoc, DataDoc["motor"]);
-    JsonArray arr = DataDoc["motor"].as<JsonArray>();
+    menuValue[0] = DataDoc["p1"];
+    menuValue[1] = DataDoc["f1"];
+    menuValue[2] = DataDoc["t1"];
+    menuValue[3] = DataDoc["p2"];
+    menuValue[4] = DataDoc["f2"];
+    menuValue[5] = DataDoc["t2"];
+    menuValue[6] = DataDoc["p3"];
+    menuValue[7] = DataDoc["f3"];
+    menuValue[8] = DataDoc["t3"];
+    menuValue[9] = DataDoc["p4"];
+    menuValue[10] = DataDoc["f4"];
+    menuValue[11] = DataDoc["t4"];
     for (int i = 0; i < menuLen; i++) {
-      menuValue[i] = arr[i];
-      if (debug) Serial.println(menuValue[i]);
+      Serial.println(menuValue[i]);
     }
   }
 
@@ -176,17 +219,28 @@ void readData() {
   }
 }
 void writeData() {
-  // create an empty array
-  JsonArray array = StepperDoc.to<JsonArray>();
-  for (int i = 0; i < menuLen; i++) {
-    array.add(menuValue[i]);
-  }
-
   DataDoc["isStore"] = 1;
-  DataDoc["motor"] = StepperDoc;
+  DataDoc["p1"] = menuValue[0];
+  DataDoc["f1"] = menuValue[1];
+  DataDoc["t1"] = menuValue[2];
+  DataDoc["p2"] = menuValue[3];
+  DataDoc["f2"] = menuValue[4];
+  DataDoc["t2"] = menuValue[5];
+  DataDoc["p3"] = menuValue[6];
+  DataDoc["f3"] = menuValue[7];
+  DataDoc["t3"] = menuValue[8];
+  DataDoc["p4"] = menuValue[9];
+  DataDoc["f4"] = menuValue[10];
+  DataDoc["t4"] = menuValue[11];
   if (debug) Serial.println("Save data to EEPROM");
   EepromStream eepromStream(0, 128);
   serializeJson(DataDoc, eepromStream);
   if (debug) serializeJsonPretty(DataDoc, Serial);
   delay(1000);
+}
+
+void sweepData() {
+  for (int i = 0; i < EEPROM.length(); i++) {
+    EEPROM.write(i, 0);
+  }
 }
