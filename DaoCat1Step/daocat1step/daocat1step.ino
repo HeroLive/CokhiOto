9977decb76815bc2c60b896d643681a31a8ea60d
Hero Live <66154935+HeroLive@users.noreply.github.com>
2023-07-16 22:34:41 +0700

    dao cat 1 step

--- DaoCat1Step/daocat1step/daocat1step.ino
+++ DaoCat1Step/daocat1step/daocat1step.ino
@@ -3,7 +3,7 @@
 #define PUL_PIN 11
 #define DIR_PIN 12
 #define EN_PIN 13
-#define RUN_PIN 10
+#define Run 10
 #define ROT_PIN A0
 
 StepperMotor stepperX(PUL_PIN, DIR_PIN);
@@ -12,53 +12,62 @@ float disPerRoundX = 360;
 float gearX = 1;
 int microStepX = 3200;
 float stepsPerUnitX = microStepX * gearX / disPerRoundX;
-float speedX = 1000;  //chinh toc do X
+long speedX = 1000;  //chinh toc do X
 
 bool debug = true;
 bool MXA = false;
 bool MXB = false;
+long t = millis();
 
 long Xa = 0;
-long Xb = 180 * stepsPerUnitX;  //vi tri X den B
+long Xb = 360 * stepsPerUnitX;  //vi tri X den B
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(EN_PIN, OUTPUT);
-  pinMode(RUN_PIN, INPUT_PULLUP);
+  pinMode(Run, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(RUN_PIN) == 0) {
-    MXA = false;
-    MXB = true;
-  }
-
-  if(!MXA && !MXB){
-    speedX = 100* analogRead(ROT_PIN);
-    Serial.println(speedX);
-  }
-
-  if (MXB) {
-    stepperX.DRVA(Xb, speedX);
-    if (stepperX.getExeCompleteFlag()) {
-      MXB = false;
-      MXA = true;
-      if (debug) {
-        Serial.print("X go to B - Position: ");        
-        Serial.println(stepperX.getCurrentPosition());
-        Serial.println(speedX);
+  if (!MXA && !MXB) {
+    speedX = 50 * long(analogRead(ROT_PIN));
+    // Serial.println(analogRead(ROT_PIN));
+    if (!digitalRead(Run)) {
+      t = millis();
+      while (!digitalRead(Run)) {
+        if (debug) {
+          Serial.println("Press Run button ");
+        }
+      }
+      if (millis() - t > 10) {
+        MXA = false;
+        MXB = true;
       }
     }
-  } else if (MXA) {
-    stepperX.DRVA(Xa, speedX);
-    if (stepperX.getExeCompleteFlag()) {
-      MXA = false;
-      if (debug) {
-        Serial.print("X go to A - Position: ");
-        Serial.println(stepperX.getCurrentPosition());
-        Serial.println(speedX);
+  } else {
+    if (MXB) {
+      stepperX.DRVA(Xb, speedX);
+      if (stepperX.getExeCompleteFlag()) {
+        MXB = false;
+        MXA = true;
+        if (debug) {
+          Serial.print("X go to B - Position: ");
+          Serial.println(stepperX.getCurrentPosition());
+          Serial.println(speedX);
+        }
+        delay(50);
+      }
+    } else if (MXA) {
+      stepperX.DRVA(Xa, speedX);
+      if (stepperX.getExeCompleteFlag()) {
+        MXA = false;
+        if (debug) {
+          Serial.print("X go to A - Position: ");
+          Serial.println(stepperX.getCurrentPosition());
+          Serial.println(speedX);
+        }
       }
     }
   }
